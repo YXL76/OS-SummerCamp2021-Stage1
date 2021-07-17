eb0af75af2a9cf06fd2b05c17d799fec17fca7b4
YXL <chenxin.lan.76@gmail.com>
2021-07-17 23:18:52 +0800

    rCore Tutorial: Init project

---
+++ os/src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+#![feature(global_asm)]
+#![feature(llvm_asm)]
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+
+global_asm!(include_str!("entry.asm"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    unsafe {
+        core::slice::from_raw_parts_mut(sbss as usize as *mut u8, ebss as usize - sbss as usize)
+            .fill(0);
+    }
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
