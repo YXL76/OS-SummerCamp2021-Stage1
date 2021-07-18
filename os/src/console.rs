886a996399a0d9b103855149db3ffdaced1edd08
YXL <chenxin.lan.76@gmail.com>
2021-07-18 12:39:25 +0800

    rCore Tutorial: Init logger

--- os/src/console.rs
+++ os/src/console.rs
@@ -1,5 +1,6 @@
 use crate::sbi::console_putchar;
 use core::fmt::{self, Write};
+use log::{self, Level, LevelFilter, Log, Metadata, Record};
 
 struct Stdout;
 
@@ -16,6 +17,12 @@ pub fn print(args: fmt::Arguments) {
     Stdout.write_fmt(args).unwrap();
 }
 
+fn print_in_color(args: fmt::Arguments, color_code: u8) {
+    Stdout
+        .write_fmt(format_args!("\u{1B}[{}m{}\u{1B}[0m", color_code, args))
+        .unwrap();
+}
+
 #[macro_export]
 macro_rules! print {
     ($fmt: literal $(, $($arg: tt)+)?) => {
@@ -27,5 +34,49 @@ macro_rules! print {
 macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+
+static LOGGER: SimpleLogger = SimpleLogger;
+
+pub fn init() {
+    log::set_logger(&LOGGER).unwrap();
+    log::set_max_level(match option_env!("LOG") {
+        Some("ERROR") => LevelFilter::Error,
+        Some("WARN") => LevelFilter::Warn,
+        Some("INFO") => LevelFilter::Info,
+        Some("DEBUG") => LevelFilter::Debug,
+        Some("TRACE") => LevelFilter::Trace,
+        _ => LevelFilter::Off,
+    });
+}
+
+struct SimpleLogger;
+
+impl Log for SimpleLogger {
+    fn enabled(&self, _: &Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &Record) {
+        /* if !self.enabled(record.metadata()) {
+            return;
+        } */
+        print_in_color(
+            format_args!("[{}] {}\n", record.level(), record.args()),
+            level_to_color_code(record.level()),
+        );
+    }
+
+    fn flush(&self) {}
+}
+
+fn level_to_color_code(level: Level) -> u8 {
+    match level {
+        Level::Error => 31, // Red
+        Level::Warn => 93,  // BrightYellow
+        Level::Info => 34,  // Blue
+        Level::Debug => 32, // Green
+        Level::Trace => 90, // BrightBlack
     }
 }
