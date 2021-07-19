d25c43a24b232fb1893a4e303521e0bc96a4a076
YXL <chenxin.lan.76@gmail.com>
2021-07-19 11:26:58 +0800

    rCore Tutorial: Complete lab1

--- os/src/main.rs
+++ os/src/main.rs
@@ -9,6 +9,8 @@ mod console;
 mod lang_items;
 mod sbi;
 
+use log::{debug, error, info, trace, warn};
+
 global_asm!(include_str!("entry.asm"));
 
 fn clear_bss() {
@@ -24,7 +26,42 @@ fn clear_bss() {
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
     clear_bss();
-    println!("Hello, world!");
+    console::init();
+    error!("hello world");
+    warn!("hello world");
+    info!("hello world");
+    debug!("hello world");
+    trace!("hello world");
+
+    let stext = stext as usize;
+    let etext = etext as usize;
+    let srodata = srodata as usize;
+    let erodata = erodata as usize;
+    let sdata = sdata as usize;
+    let edata = edata as usize;
+    let boot_stack = boot_stack as usize;
+    let boot_stack_top = boot_stack_top as usize;
+    let sbss = sbss as usize;
+    let ebss = ebss as usize;
+
+    info!(".text [{:#x}, {:#x})", stext, etext);
+    info!(".rodata [{:#x}, {:#x})", srodata, erodata);
+    info!(".data [{:#x}, {:#x})", sdata, edata);
+    info!("boot_stack [{:#x}, {:#x})", boot_stack, boot_stack_top);
+    info!(".bss [{:#x}, {:#x})", sbss, ebss);
+
     panic!("Shutdown machine!");
 }
