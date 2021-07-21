187beee1691afcad7ec8b08c049b7bd06a93f018
YXL <chenxin.lan.76@gmail.com>
2021-07-21 13:41:22 +0800

    rCore Tutorial: change `sys_get_time`

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,6 @@
-use crate::task::{exit_current_and_run_next, suspend_current_and_run_next};
-use crate::timer::get_time_ms;
+use crate::mm::translated_byte_buffer;
+use crate::task::{current_user_token, exit_current_and_run_next, suspend_current_and_run_next};
+use crate::timer::get_time_us;
 
 pub fn sys_exit(exit_code: i32) -> ! {
     info!("[kernel] Application exited with code {}", exit_code);
@@ -12,6 +13,20 @@ pub fn sys_yield() -> isize {
     0
 }
 
-pub fn sys_get_time() -> isize {
-    get_time_ms() as isize
+pub fn sys_get_time(ts: *const u8, _tz: usize) -> isize {
+    let ts =
+        translated_byte_buffer(current_user_token(), ts, 1)[0].as_ptr() as *const _ as *mut TimeVal;
+    let usec = get_time_us();
+    unsafe {
+        (*ts).sec = usec / 1000000;
+        (*ts).usec = usec % 1000000;
+    }
+    0
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct TimeVal {
+    pub sec: usize,
+    pub usec: usize,
 }
